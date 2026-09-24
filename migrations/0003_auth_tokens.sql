0b462371ca408a03cee9bccc1ac45397632ca8cf
Egzotik <egz0tikys99@gmail.com>
2026-09-24 16:46:43 +0300

    Public repo is now redirect stubs only

--- migrations/0003_auth_tokens.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE users ADD COLUMN access_token TEXT;
-ALTER TABLE users ADD COLUMN refresh_token TEXT;
-ALTER TABLE users ADD COLUMN token_expires_at TEXT;
-ALTER TABLE users ADD COLUMN role_checked_at TEXT;
